b922770a1bb177eb55a22fc335f36bc9d873b537
backbay2 <backbay2@163.com>
2023-03-15 23:47:07 +0800

    Initial commit

---
+++ src/main/java/com/backbay2/kolasecurity/KolaSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.backbay2.kolasecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolaSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolaSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backbay2/kolasecurity/KolaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backbay2.kolasecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolaSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
